00ea29f62bed597d9396bfcde995c36becdb2069
Imran Khan <57177823+imran-svg@users.noreply.github.com>
2020-05-12 12:28:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_lb" "this" {
   }
 }
 
-resource "aws_lb" "main" {
+resource "aws_lb1" "main" {
   count = var.create_lb ? length(var.target_groups) : 0
 
   name        = lookup(var.target_groups[count.index], "name", null)
